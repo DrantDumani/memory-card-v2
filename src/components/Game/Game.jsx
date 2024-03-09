0cde737f5e7f1c35e1e317e882b2518404307440
DrantDumani <darnjames@gmail.com>
2024-03-09 15:05:27 -0500

    Complete mobile version of stylesheet

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -4,12 +4,7 @@ import Card from "../Card/Card";
 function Game({ level, cards, handleClick }) {
   return (
     <>
-      <p className="main__rules">
-        <span className="main__rules--bold">Rules: </span>Click on a card, but
-        don&apos;t click the same card twice per level.
-      </p>
-
-      <h2 className="main__level">Level {level}</h2>
+      <h2 className="game-level">Level {level}</h2>
 
       <div className="card-grid">
         {cards.map((card) => (
